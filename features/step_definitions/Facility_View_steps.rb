10df5a9a9249e58ad2f6cf9080e0a587c5fc41eb
Chandrakala.Gattu <cgattu@raygroupintl.com>
2014-12-22 10:17:43 -0500

    Test

--- features/step_definitions/Facility_View_steps.rb
+++ features/step_definitions/Facility_View_steps.rb
@@ -73,9 +73,9 @@ When(/^I click on Widgets$/) do
   
 end
 
+
 Then(/^I should see  "(.*?)"$/) do |arg1|
- #pending # express the regexp above with the code you wish you had
- 
- expect(page).to have_content(arg1) 
- sleep(10)
+  #pending # express the regexp above with the code you wish you had
+  expect(page).to have_content(arg1) 
 end
+
